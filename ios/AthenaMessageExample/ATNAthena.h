16aa6723a2a6fb4be3232be9285f59f5395d5710
Leon <Leon4w@outlook.com>
2017-09-26 14:08:06 +0800

    [NEW] Declare Athena.

---
+++ ios/AthenaMessageExample/ATNAthena.h
@@ -0,0 +1,13 @@
+//
+//  ATNAthena.h
+//  AthenaMessageExample
+//
+//  Created by Leon Tse on 2017/9/26.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATNAthena : NSObject
+
+@end
---
+++ ios/AthenaMessageExample/ATNAthena.m
@@ -0,0 +1,15 @@
+//
+//  ATNAthena.m
+//  AthenaMessageExample
+//
+//  Created by Leon Tse on 2017/9/26.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "ATNAthena.h"
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+
+@implementation ATNAthena
+
+@end
